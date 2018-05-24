a0a78dc074060aa65657576491e99751409c83ee
Henry Stanley <henry@henrystanley.com>
2018-05-24 11:12:00 -0400

    improve metadata file decoding

--- loader/loader.go
+++ loader/loader.go
@@ -1,15 +1,17 @@
 package loader
 
 import (
-	"errors"
+	"bytes"
+	"crypto/sha1"
 	"os"
 
+	"github.com/go-errors/errors"
 	bencode "github.com/jackpal/bencode-go"
 )
 
 type TorrentLoader interface {
 	Load(string) error
-	Decode() (map[string]interface{}, error)
+	Decode() (map[string]interface{}, []byte, error)
 }
 
 type TorrentLoaderImpl struct {
@@ -25,23 +27,38 @@ func (f *TorrentLoaderImpl) Load(path string) error {
 	f.path = path
 	torrentFile, err := os.Open(path)
 	if err != nil {
-		return err
+		return errors.Wrap(err, 0)
 	}
 
 	f.file = torrentFile
 	return nil
 }
 
-func (f *TorrentLoaderImpl) Decode() (map[string]interface{}, error) {
+func (f *TorrentLoaderImpl) Decode() (map[string]interface{}, []byte, error) {
 	m, err := bencode.Decode(f.file)
 	if err != nil {
-		return nil, err
+		return nil, nil, errors.Wrap(err, 0)
 	}
 
 	metadataMap, ok := m.(map[string]interface{})
 	if !ok {
-		return nil, errors.New("Metadata couldn't be read as a map - likely invalid")
+		return nil, nil, errors.Wrap(errors.New("Metadata couldn't be read as a map - likely invalid"), 1)
 	}
 
-	return metadataMap, nil
+	hash, err := calculateInfoHash(metadataMap)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, 0)
+	}
+
+	return metadataMap, hash, nil
+}
+
+func calculateInfoHash(info map[string]interface{}) ([]byte, error) {
+	encodedInfo := new(bytes.Buffer)
+	bencode.Marshal(encodedInfo, info)
+
+	hash := sha1.New()
+	hash.Write(encodedInfo.Bytes())
+
+	return hash.Sum(nil), nil
 }
